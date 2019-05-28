8a4a8bf0fe6a8ee4fd339e778313e35f1e3e5114
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-05-28 14:01:32 -0600

    show user's info test

--- dist/js/redemption.js
+++ dist/js/redemption.js
@@ -1,19 +1,33 @@
 
-const BADGR_ISSUER_ID = "";
+const BADGR_ISSUER_ID = "rGy5MNWtQgSs1vfnLyPlmg";
+const BADGR_ACCESS_TOKEN = "irLWj7bSsXb2Ln4vsTooyFSfSmwOSG";
 const BADGR_COURSE_TYPE = "course";
 const BADGR_EPIPHANY_TYPE = "epiphany";
 const BADGR_REDEMPTION_TYPE = "redemption";
+const BADGR_BASE_URL = "https://api.badgr.io/";
+const BADGR_SERVER_SLUG_EPIPHANY = "V_MaSinhQJeKGOtZz6tDAQ";
+const BADGR_SERVER_SLUG_REDEMPTION = "XrG4QUcyTQGVch1VipS-Qw";
 
-var BADGR_BASE_URL = "https://api.badgr.io/";
+
 var BADGR_BADGECLASS_SINGLE_ISSUER_PATH = "v2/issuers/{0}/badgeclasses"       // issuer id
 var BADGR_ASSERTION_BADGECLASS_PATH = "v2/badgeclasses/{0}/assertions";            // badge_class entityId
 var BADGR_ASSERTION_ISSUER_PATH = "v2/issuers/{0}/assertions";
+
 
 var recipient = new Object();
 recipient.identity = "string";
 recipient.type = "email";
 recipient.hashed = true;
 recipient.plaintextIdentity = "string";
+
+        // EPIPHANY BADGE SERVER SLUG: V_MaSinhQJeKGOtZz6tDAQ
+        // IMAGE: https: // media.us.badgr.io / uploads / badges / issuer_badgeclass_efc20af1 - 7d43 - 4d1e - 877e-447244ea3fd3.png
+
+        // COURSE BADGE SERVER SLUG: 2gnNK3RZSlOutOrVeQlD_A
+        // IMAGE: https: // media.us.badgr.io / uploads / badges / issuer_badgeclass_63237c1a - 3f3d - 40b7 - 9e48 - 085658d2799f.png
+
+        // REDEMPTION BADGE SERVER SLUG: XrG4QUcyTQGVch1VipS-Qw
+        // IMAGE: https: // media.us.badgr.io / uploads / badges / issuer_badgeclass_41b742a0 - d58c - 4223 - bffb - f2bc92fdd4bf.png
 
 
 // UTITLITIES
@@ -47,11 +61,21 @@ function print(fmt, ...args) {
 
 
 function setVarsGlobally(vars) {
-  window.username = vars["username"];
-  window.useremail = vars["useremail"];
-  window.num_epiph_asserts = vars["num_epiph_asserts"];
-  window.epiphany_badgeclass_id = vars["epiphany_badgeclass_id"];
-  window.epiphany_issuer_id = vars["epiphany_issuer_id"];
+  window.username = vars.username;
+  window.useremail = vars.useremail;
+  window.epiphany_badgeclass_id = vars.epiphany_badgeclass_id;
+  window.epiphany_issuer_id = vars.epiphany_issuer_id;
+}
+
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
 }
 
 function getUrlVars() {
@@ -62,12 +86,14 @@ function getUrlVars() {
         //   username: "",
         //   useremail: ""
         // };
+  var pc_pkg = JSON.parse(findGetParameter("pc_pkg_str"));
+
   var vars = {
     num_epiph_asserts: 5,
-    epiphany_badgeclass_id: "",
+    epiphany_badgeclass_id: pc_pkg.epiphany_badgeclass_id,
     epiphany_issuer_id: "rGy5MNWtQgSs1vfnLyPlmg",
-    username: "admin",
-    useremail: "peter.alexander@prodatalab.com"
+    username: pc_pkg.username,
+    useremail: pc_pkg.useremail
   };
   // var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
   //   vars[key] = value;
@@ -92,11 +118,6 @@ function getBadgeClasses() {
   // https://www.w3schools.com/xml/ajax_xmlhttprequest_send.asp
   //   return makeHttpRequest(BADGR_BADGECLASS_SINGLE_ISSUER_PATH, "GET");
   var xhttp = new XMLHttpRequest();
-  // "Authorization: Bearer Z2iZwHMpQtOZtueS4mSNAuz6AmQMZX";
-  // {
-  // "badgr_access_token": "hJPjyjIiIpiCiaCsBkZEkM3Su4rc1R",
-  // "badgr_refresh_token": "2Yur56qYRfGH7K8zZMbkGvgRA6Aq3E"
-  // } 
 
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -106,21 +127,9 @@ function getBadgeClasses() {
     }
   };
   xhttp.open("GET", BADGR_BASE_URL + format(BADGR_BADGECLASS_SINGLE_ISSUER_PATH, "rGy5MNWtQgSs1vfnLyPlmg"));
-  xhttp.setRequestHeader("Authorization", "Bearer irLWj7bSsXb2Ln4vsTooyFSfSmwOSG");
+  xhttp.setRequestHeader("Authorization", "Bearer " + BADGR_ACCESS_TOKEN);
   xhttp.send();
 }
-
-
-        // data = {
-        //     'recipient': {
-        //         'identity': uname,
-        //         'type': 'email',
-        //         'hashed': False,
-        //         'plaintextIdentity': ptid
-        //     }
-        // }
-
-
 
 
 function getAssertions() {
@@ -130,8 +139,8 @@ function getAssertions() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       ret = JSON.parse(this.responseText)
-      // document.getElementById("demo").innerHTML = ret.result[0].recipient.identity;
-      document.getElementById("demo").innerHTML = window.useremail;
+      document.getElementById("demo").innerHTML = this.responseText;
+    //   document.getElementById("demo").innerHTML = window.useremail;
     } else {
       console.log("WHAT THE FUCK: " + this.status + " " + this.responseText);
     }
@@ -139,15 +148,15 @@ function getAssertions() {
 
   var params = "recipient=peter.alexander@prodatalab.com"
 
-  xhttp.open("GET", BADGR_BASE_URL + format(BADGR_ASSERTION_ISSUER_PATH, window.epiphany_issuer_id) + "?" + params);
-  xhttp.setRequestHeader("Authorization", "Bearer irLWj7bSsXb2Ln4vsTooyFSfSmwOSG");
+  xhttp.open("GET", BADGR_BASE_URL + format(BADGR_ASSERTION_ISSUER_PATH, BADGR_ISSUER_ID) + "?" + params);
+  xhttp.setRequestHeader("Authorization", "Bearer " + BADGR_ACCESS_TOKEN);
   xhttp.send();
 }
 
 
 
-function displayUsersInfo() {
-  
+function displayUserInfo() {
+  document.getElementById("demo").innerHTML = window.username + ";" + window.useremail + ";" + window.epiphany_issuer_id + ";" + window.epiphany_badgeclass_id;
 }
 
 
