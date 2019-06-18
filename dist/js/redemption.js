2b279b91fbe2f60730cabf712200741204067367
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-06-17 20:03:13 -0600

    num-epiph-fixed6

--- dist/js/redemption.js
+++ dist/js/redemption.js
@@ -404,6 +404,7 @@ function onSelectPrizeEvent(title) {
   // $("num-spent-input").val(assertions.result.length);
   // ep_spent = document.getElementById("num-spent-input").value;
   document.getElementById("num-spent-input").setAttribute("max", window.num_epiph_asserts);
+  document.getElementById("num-spent-input").value = 0
   window.selectedPrize = convertToSlug(title);
   $("#placeBidModal").modal();
 }
