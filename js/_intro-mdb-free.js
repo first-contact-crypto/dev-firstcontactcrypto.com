7982501a2f022d099cb9881f1a9cca5e68d251b8
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-07-25 09:49:02 -0600

    new gulp environment installed

---
+++ js/_intro-mdb-free.js
@@ -0,0 +1,41 @@
+/*!
+ * Material Design for Bootstrap 4
+ * Version: MDB FREE: 4.8.7
+ *
+ *
+ * Copyright: Material Design for Bootstrap
+ * https://mdbootstrap.com/
+ *
+ * Read the license: https://mdbootstrap.com/general/license/
+ *
+ *
+ * Documentation: https://mdbootstrap.com/
+ *
+ * Getting started: https://mdbootstrap.com/docs/jquery/getting-started/download/
+ *
+ * Tutorials: https://mdbootstrap.com/education/bootstrap/
+ *
+ * Templates: https://mdbootstrap.com/templates/
+ *
+ * Support: https://mdbootstrap.com/forums/forum/support/
+ *
+ * Contact: office@mdbootstrap.com
+ *
+ * Attribution: Animate CSS, Twitter Bootstrap, Materialize CSS, Normalize CSS, Waves JS, WOW JS, Toastr, Chart.jss
+ *
+ */
+
+
+/*
+
+  jquery.easing.js
+  velocity.min.js
+  chart.js
+  wow.js
+  scrolling-navbar.js
+  waves.js
+  forms-free.js
+  enhanced-modals.js
+  treeview.js
+
+*/
---
+++ js/modules.js
@@ -0,0 +1,19 @@
+exports.modules = [
+  './js/_intro-mdb-free.js',
+  './js/vendor/jquery.easing.js',
+  './js/vendor/velocity.js',
+  './js/vendor/chart.js',
+  './js/vendor/wow.js',
+  './js/dist/scrolling-navbar.js',
+  './js/vendor/waves.js',
+  './js/dist/forms-free.js',
+  './js/vendor/enhanced-modals.js',
+  './js/dist/treeview.js',
+  './js/vendor/bs-custom-file-input.js'
+  // './js/vendor/addons/jquery.zmd.hierarchical-display.js/'
+  // './js/vendor/addons/masonry.pkgd.min.js',
+  // './js/vendor/addons/imagesloaded.pkgd.min.js'
+  // './js/vendor/addons/datatables.js'
+  // './js/vendor/addons/rating.js'
+  // './js/vendor/addons/progressBar.js'
+];
